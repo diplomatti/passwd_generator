fb359d0b8fab717eecd5e1f0943c955292afebae
Ville-Matti Rantamaa <100934967+diplomatti@users.noreply.github.com>
2023-03-29 20:28:02 +0300

    version 0.2.1

--- generator.py
+++ generator.py
@@ -4,28 +4,26 @@ import string
 running = True
 
 print()
-print("------------------------")
-print("~ passwd_generator v0.2~")
-print("------------------------")
+print("--------------------------")
+print("~ passwd_generator v0.2.1~")
+print("--------------------------")
 print()
 
+# Assigining letters digits and characters to variable
+alphabet = string.ascii_letters + string.digits + string.punctuation
+pwd = alphabet
+
+passwd_length = 15
 
 while running:
-
-
-    letters = string.ascii_letters
-    digits = string.digits
-    characters = string.punctuation
-
-    passwd_length = 15
-    alphabet = letters + digits + characters
-    pwd = alphabet
-
+    # Empty list for password
     password = []
     
+    # Input from user wether to generate new password or exit program
     choice = (int(input("What you want to do?\n1. Generate password\n2. Exit program\n")))
     print()
 
+    # If new password
     if choice == 1:
         for i in range(passwd_length):
             randomchar = secrets.choice(pwd)
@@ -36,9 +34,10 @@ while running:
         print()
         continue
 
+    # If exiting the program
     elif choice == 2:
         running = False
 
 print()
-print("Thank you for using passwd_generator")
+print("Thank you for using passwd_generator!")
 
