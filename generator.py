c67eaf6c315e8216a5c3a36e7f4373df28568fc3
Ville-Matti Rantamaa <100934967+diplomatti@users.noreply.github.com>
2023-04-10 16:29:47 +0300

    v0.2.2

--- generator.py
+++ generator.py
@@ -1,37 +1,56 @@
 import secrets
 import string
+# import os
 
 running = True
+ready = True
 
 print()
 print("--------------------------")
-print("~ passwd_generator v0.2.1~")
+print("~ passwd_generator v0.2.2~")
 print("--------------------------")
 print()
-
-# Assigining letters digits and characters to variable
-alphabet = string.ascii_letters + string.digits + string.punctuation
-pwd = alphabet
 
 passwd_length = 15
+
+# Clear the terminal screen
+# os.system('cls' if os.name == 'nt' else 'clear')
 
 while running:
     # Empty list for password
     password = []
-    
+    pwd = ""
+
     # Input from user wether to generate new password or exit program
     choice = (int(input("What you want to do?\n1. Generate password\n2. Exit program\n")))
     print()
 
     # If new password
     if choice == 1:
+        print("Choose password content from these:\n1. Letters\n2. Digits\n3. Special characters\n4. Continue")
+        
+        while ready:
+            pw_contents = int(input())
+            if pw_contents == 1:
+                pwd += string.ascii_letters
+            elif pw_contents == 2:
+                pwd += string.digits
+            elif pw_contents == 3:
+                pwd += string.punctuation
+            elif pw_contents == 4:
+                break
+            else:
+                print("Please pick a valid option")
+            
         for i in range(passwd_length):
             randomchar = secrets.choice(pwd)
             password.append(randomchar)
-            
+
+        print("-------------------------------------------------")
         print("Your super secret password is: " + "".join(password))
         print("-------------------------------------------------")
         print()
+
         continue
 
     # If exiting the program
